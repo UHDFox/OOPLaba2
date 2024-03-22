ab46045e296694e06dd228047c20611185927663
UHDFox <uhdfox@yandex.ru>
2024-03-22 16:19:07 +0300

    .

---
+++ Client/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Client;
+
+class Program
+{
+    static async Task Main()
+    {
+        var client = new Client();
+        await client.Connect(80);
+        await client.SendDataAsync("capybaraluv");
+        await client.SendDataAsync("orange");
+        client.CloseConnection();
+    }
+}
